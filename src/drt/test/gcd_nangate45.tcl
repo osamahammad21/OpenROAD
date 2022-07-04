8bd5860ffa66e15f16270d36382747bf16805852
osamahammad21 <osama21@aucegypt.edu>
2022-07-04 13:40:00 +0200

    drt: split end
    Signed-off-by: osamahammad21 <osama21@aucegypt.edu>

--- src/drt/test/gcd_nangate45.tcl
+++ src/drt/test/gcd_nangate45.tcl
@@ -2,9 +2,8 @@ read_lef Nangate45/Nangate45_tech.lef
 read_lef Nangate45/Nangate45_stdcell.lef
 read_def gcd_nangate45_preroute.def
 set_thread_count [expr [exec getconf _NPROCESSORS_ONLN] / 4]
-detailed_route -guide gcd_nangate45.route_guide \
-               -output_guide results/gcd_nangate45.output.guide.mod \
-               -output_drc results/gcd_nangate45.output.drc.rpt \
+read_guides  gcd_nangate45.route_guide
+detailed_route -output_drc results/gcd_nangate45.output.drc.rpt \
                -output_maze results/gcd_nangate45.output.maze.log \
                -verbose 1 
 set def_file results/gcd_nangate45.defok
